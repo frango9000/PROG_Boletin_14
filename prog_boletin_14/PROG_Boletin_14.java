4e8164637e11257c705933c4635e4ca731f90da6
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2018-12-12 12:05:58 +0100

    pedimos grados C devolvemos temperatura en F y R

--- prog_boletin_14/PROG_Boletin_14.java
+++ prog_boletin_14/PROG_Boletin_14.java
@@ -5,6 +5,7 @@
  */
 package prog_boletin_14;
 
+import java.util.Scanner;
 /**
  *
  * @author fsancheztemprano
@@ -15,7 +16,26 @@ public class PROG_Boletin_14 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        Scanner scan = new Scanner(System.in);
+        ConversorTemperaturas ct = new ConversorTemperaturas();
+        boolean valid = false;
+        float f=0f;
+        float r=0f;
+        do{
+            System.out.println("Introduce temp en Cº: ");
+            float c = scan.nextFloat();
+            try{
+                f = ct.temperaturaAFarenheit(c);
+                r = ct.temperaturaAReamur(c);
+                valid=true;
+                System.out.println(c+"ºC a Farenheit: "+f);
+                System.out.println(c+"ºC a Reamur: "+r);
+            }catch (TemperaturaErradaExcepcion e){
+                System.out.println(e.getMessage());
+            }
+            
+        }while(!valid);
     }
     
 }
